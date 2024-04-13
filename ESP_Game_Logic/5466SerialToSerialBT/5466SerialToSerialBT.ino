b54efec218a6308ec536670019ec26556688e984
ziegler-287 <145054611+ziegler-287@users.noreply.github.com>
2024-04-13 19:37:31 -0400

    Somewhat Stable game version. Before adding ESP-Now connection

--- ESP_Game_Logic/5466SerialToSerialBT/5466SerialToSerialBT.ino
+++ ESP_Game_Logic/5466SerialToSerialBT/5466SerialToSerialBT.ino
@@ -13,13 +13,32 @@ String device_name = "ESP32-Game-Manager";
 
 int game = 0;
 
-const byte BUTTON_0 = 18;
-const byte BUTTON_1 = 19;
-const byte BUTTON_2 = 21;
-const byte BUTTON_3 = 17;
-const byte BUTTON_4 = 5;
+const byte BUTTON_0_IN = 34;
+const byte BUTTON_1_IN = 32;
+const byte BUTTON_2_IN = 33;
+const byte BUTTON_3_IN = 39;
+const byte BUTTON_4_IN = 5;
+const byte BUTTON_0_OUT = 18;
+const byte BUTTON_1_OUT = 19;
+const byte BUTTON_2_OUT = 17;
+const byte BUTTON_3_OUT = 22;
+const byte BUTTON_4_OUT = 5;
 
-int delay_time;
+
+int delay_time = 500;
+int reaction_time = 20000;
+int score = 0;
+int failed = 0;
+bool correct = true;
+bool newButton = false;
+int i = 0;
+int button = -1;
+int recievedMessage = 0;
+int old_button = -2;
+
+int button_0_pressed = 0;
+int button_1_pressed = 0;
+int button_2_pressed = 0;
 
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
@@ -40,43 +59,291 @@ void setup() {
     SerialBT.setPin(pin);
     Serial.println("Using PIN");
   #endif
-  pinMode(BUTTON_0, OUTPUT);
-  pinMode(BUTTON_1, OUTPUT);
-  pinMode(BUTTON_2, OUTPUT);
-  pinMode(BUTTON_3, OUTPUT);
-  pinMode(BUTTON_4, OUTPUT);
+  pinMode(BUTTON_0_OUT, OUTPUT);
+  pinMode(BUTTON_1_OUT, OUTPUT);
+  pinMode(BUTTON_2_OUT, OUTPUT);
+  pinMode(BUTTON_3_OUT, OUTPUT);
+  pinMode(BUTTON_4_OUT, OUTPUT);
+  pinMode(BUTTON_0_IN, INPUT);
+  pinMode(BUTTON_1_IN, INPUT);
+  pinMode(BUTTON_2_IN, INPUT);
+
+  Serial.setTimeout(50);
+  SerialBT.setTimeout(100);
 }
 
 void loop() {
-  if (Serial.available()) {
+ /* if (Serial.available()) {
     SerialBT.write(Serial.read());
   }
-  if (SerialBT.available()) {
-    //Serial.write(SerialBT.read());
-    String recievedMessage = SerialBT.readStringUntil('\n');
-    if (recievedMessage.equals("1play")) {
+  */
+  if (Serial.available()) {
+    int recievedRead = Serial.parseInt();
+    Serial.println(recievedRead);
+    if (recievedRead != 0) {
+      recievedMessage = recievedRead;
+    }
+    Serial.println(recievedMessage);
+    if (recievedMessage == 10) {
+      game = 2;
+      failed = 0;
+      i = 0;
+      recievedMessage = 0;
+      Serial.println("Start 2 Player");
+    }
+    if (recievedMessage == 9) {
       game = 1;
+      failed = 0;
+      i = 0;
+      recievedMessage = 0;
+    }
+    if (recievedMessage == 1) {
+        button = 0;
+        i = 0;
+        Serial.println("button 1 on");
+      }
+      else if (recievedMessage == 2) {
+        i = 0;
+        button = 1;
+        Serial.println("button 2 on");
+      }
+      else if (recievedMessage == 3) {
+        i = 0;
+        button = 2;
+        Serial.println("button 3 on");
+      }
+  }
+  if (SerialBT.available()) {
+    newButton = false;
+    int recievedRead = SerialBT.parseInt();
+    Serial.println(recievedRead);
+    if (recievedRead != 0) {
+      recievedMessage = recievedRead;
+      newButton = true;
+    }
+    Serial.println(recievedMessage);
+    if (recievedMessage == 10) {
+      game = 2;
+      failed = 0;
+      i = 0;
+      recievedMessage = 0;
+      Serial.println("Start 2 Player");
+    }
+    if (recievedMessage == 9) {
+      game = 1;
+      failed = 0;
+      i = 0;
+      recievedMessage = 0;
+    }
+    if (recievedMessage == 1) {
+      if (button != -1 && newButton) {
+        failed++;
+        }
+        button = 0;
+        Serial.println("button 1 on");
+        i = 0;
+       }
+      else if (recievedMessage == 2) {
+        if (button != -1 && newButton) {
+        failed++;
+        }
+        button = 1;
+        Serial.println("button 2 on");
+        i = 0;
+       }
+      else if (recievedMessage == 3) {
+        if (button != -1 && newButton) {
+        failed++;
+        }
+        button = 2;
+        Serial.println("button 3 on");
+        i = 0;
+      }
+  }
+
+//1-player game
+  if (game == 1 && failed < 2) {
+    button_0_pressed = digitalRead(BUTTON_0_IN);
+    button_1_pressed = digitalRead(BUTTON_1_IN);
+    button_2_pressed = digitalRead(BUTTON_2_IN);
+    if (i < reaction_time) {
+      if (button == 0) {
+        digitalWrite(BUTTON_0_OUT, HIGH);
+        digitalWrite(BUTTON_1_OUT, LOW);
+        digitalWrite(BUTTON_2_OUT, LOW);
+      }
+      else if (button == 1) {
+        digitalWrite(BUTTON_1_OUT, HIGH);
+        digitalWrite(BUTTON_0_OUT, LOW);
+        digitalWrite(BUTTON_2_OUT, LOW);
+      }
+      else if (button == 2) {
+        digitalWrite(BUTTON_0_OUT, LOW);
+        digitalWrite(BUTTON_1_OUT, LOW);
+        digitalWrite(BUTTON_2_OUT, HIGH);
+      }
+      if (button_0_pressed == HIGH) {
+        if (button == 0) {
+          i = 0;
+          digitalWrite(BUTTON_0_OUT, LOW);
+          score++;
+          delay(delay_time);
+          button = random(3);
+          delay(25);
+        }
+        else {
+          button_miss();
+          button = random(3);
+          delay(25);
+        }
+      }
+      if (button_1_pressed == HIGH) {
+        if (button == 1) {
+          i = 0;
+          digitalWrite(BUTTON_1_OUT, LOW);
+          delay(delay_time);
+          score++;
+          button = random(3);
+          delay(25);
+        }
+        else{
+          button_miss();
+          button = random(3);
+          delay(25);
+        }
+      }
+      if (button_2_pressed == HIGH){
+        if (button == 2) {
+          i = 0;
+          digitalWrite(BUTTON_2_OUT, LOW);
+          delay(delay_time);
+          score++;
+          button = random(3);
+          delay(25);
+        }
+        else{
+          button_miss();
+          button = random(3);
+          delay(25);
+        }
+      }
+      i++;
+      //Serial.println(i);
+    }
+    else if (i == reaction_time) {
+    SerialBT.write('2');
+    digitalWrite(BUTTON_0_OUT, LOW);
+    digitalWrite(BUTTON_1_OUT, LOW);
+    digitalWrite(BUTTON_2_OUT, LOW);
+    i++;
     }
   }
 
-  if (game == 1) {
-    int targetButton = random(5);
-    if (targetButton == 0) {
-      digitalWrite(BUTTON_0, HIGH);
-    }
-    else if (targetButton == 1) {
-      digitalWrite(BUTTON_1, HIGH);
-    }
-    else if (targetButton == 2) {
-      digitalWrite(BUTTON_2, HIGH);
-    }
-    else if (targetButton == 3) {
-      digitalWrite(BUTTON_3, HIGH);
-    }
-    else if (targetButton == 4) {
-      digitalWrite(BUTTON_4, HIGH);
-    }
+
+//2 Player Game
+  if (game == 2 && failed < 2) {
+    button_0_pressed = digitalRead(BUTTON_0_IN);
+    button_1_pressed = digitalRead(BUTTON_1_IN);
+    button_2_pressed = digitalRead(BUTTON_2_IN);
     
+    if (i < reaction_time) {
+      Serial.print(button);
+      Serial.println(" should be on");
+      if (button == 0) {
+        digitalWrite(BUTTON_0_OUT, HIGH);
+        digitalWrite(BUTTON_1_OUT, LOW);
+        digitalWrite(BUTTON_2_OUT, LOW);
+      }
+      else if (button == 1) {
+        digitalWrite(BUTTON_1_OUT, HIGH);
+        digitalWrite(BUTTON_0_OUT, LOW);
+        digitalWrite(BUTTON_2_OUT, LOW);
+      }
+      else if (button == 2) {
+        digitalWrite(BUTTON_0_OUT, LOW);
+        digitalWrite(BUTTON_1_OUT, LOW);
+        digitalWrite(BUTTON_2_OUT, HIGH);
+      }
+      if (button_0_pressed == HIGH) {
+        if (button == 0) {
+          i = 0;
+          button = -1;
+          digitalWrite(BUTTON_0_OUT, LOW);
+          score++;
+          SerialBT.write(score);
+          delay(500);
+        }
+        else {
+          button_miss();
+          button = -1;
+        }
+      }
+      if (button_1_pressed == HIGH) {
+        if (button == 1) {
+          i = 0;
+          button = -1;
+          digitalWrite(BUTTON_1_OUT, LOW);
+          delay(500);
+          score++;
+          SerialBT.write(score);
+        }
+        else {
+          button_miss();
+          button = -1;
+        }
+      }
+      if (button_2_pressed == HIGH){
+        if (button == 2) {
+          i = 0;
+          button = -1;
+          digitalWrite(BUTTON_2_OUT, LOW);
+          delay(500);
+          score++;
+          SerialBT.write(score);
+        }
+        else {
+          button_miss();
+          button = -1;
+        }
+      }
+      i++;
+      //Serial.println(i);
+    }
+    else if (i == reaction_time) {
+    SerialBT.write('g');
+    digitalWrite(BUTTON_0_OUT, LOW);
+    digitalWrite(BUTTON_1_OUT, LOW);
+    digitalWrite(BUTTON_2_OUT, LOW);
+    i++;
+    }
   }
-  delay(200);
+  
+}
+
+void button_miss() {
+     failed += 1;
+     SerialBT.write('m');
+     /*
+     digitalWrite(BUTTON_0_OUT, LOW);
+     digitalWrite(BUTTON_1_OUT, LOW);
+     digitalWrite(BUTTON_2_OUT, LOW);
+     delay(100);
+     digitalWrite(BUTTON_0_OUT, HIGH);
+     digitalWrite(BUTTON_1_OUT, HIGH);
+     digitalWrite(BUTTON_2_OUT, HIGH);
+     delay(100);
+     digitalWrite(BUTTON_0_OUT, LOW);
+     digitalWrite(BUTTON_1_OUT, LOW);
+     digitalWrite(BUTTON_2_OUT, LOW);
+     */
+}
+
+void button_correct() {
+  i = 0;
+  button = -1;
+  score++;
+  delay(delay_time);
+  if (score % 5 == 0) {
+    reaction_time -= 500;
+  }
 }
