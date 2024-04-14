b80052f406f6ec531eb0a0b72f99d23f71b9976c
Ziegler <ziegler.287@coeit.osu.edu>
2024-04-13 22:57:19 -0400

    Full Game Logic for 8 buttons

--- ESP_Game_Logic/5466SerialToSerialBT/5466SerialToSerialBT.ino
+++ ESP_Game_Logic/5466SerialToSerialBT/5466SerialToSerialBT.ino
@@ -17,15 +17,21 @@ const byte BUTTON_0_IN = 34;
 const byte BUTTON_1_IN = 32;
 const byte BUTTON_2_IN = 33;
 const byte BUTTON_3_IN = 39;
-const byte BUTTON_4_IN = 5;
+const byte BUTTON_4_IN = 36;
+const byte BUTTON_5_IN = 35;
+const byte BUTTON_6_IN = 25;
+const byte BUTTON_7_IN = 26;
 const byte BUTTON_0_OUT = 18;
 const byte BUTTON_1_OUT = 19;
 const byte BUTTON_2_OUT = 17;
-const byte BUTTON_3_OUT = 22;
+const byte BUTTON_3_OUT = 16;
 const byte BUTTON_4_OUT = 5;
+const byte BUTTON_5_OUT = 4;
+const byte BUTTON_6_OUT = 0;
+const byte BUTTON_7_OUT = 2;
 
 
-int delay_time = 500;
+int delay_time = 100;
 int reaction_time = 20000;
 int score = 0;
 int failed = 0;
@@ -39,6 +45,11 @@ int old_button = -2;
 int button_0_pressed = 0;
 int button_1_pressed = 0;
 int button_2_pressed = 0;
+int button_3_pressed = 0;
+int button_4_pressed = 0;
+int button_5_pressed = 0;
+int button_6_pressed = 0;
+int button_7_pressed = 0;
 
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
@@ -64,9 +75,17 @@ void setup() {
   pinMode(BUTTON_2_OUT, OUTPUT);
   pinMode(BUTTON_3_OUT, OUTPUT);
   pinMode(BUTTON_4_OUT, OUTPUT);
+  pinMode(BUTTON_5_OUT, OUTPUT);
+  pinMode(BUTTON_6_OUT, OUTPUT);
+  pinMode(BUTTON_7_OUT, OUTPUT);
   pinMode(BUTTON_0_IN, INPUT);
   pinMode(BUTTON_1_IN, INPUT);
   pinMode(BUTTON_2_IN, INPUT);
+  pinMode(BUTTON_3_IN, INPUT);
+  pinMode(BUTTON_4_IN, INPUT);
+  pinMode(BUTTON_5_IN, INPUT);
+  pinMode(BUTTON_6_IN, INPUT);
+  pinMode(BUTTON_7_IN, INPUT);
 
   Serial.setTimeout(50);
   SerialBT.setTimeout(100);
@@ -77,6 +96,7 @@ void loop() {
     SerialBT.write(Serial.read());
   }
   */
+  if (game > 0 && failed > 1) { game_over(); }
   if (Serial.available()) {
     int recievedRead = Serial.parseInt();
     Serial.println(recievedRead);
@@ -96,22 +116,74 @@ void loop() {
       failed = 0;
       i = 0;
       recievedMessage = 0;
+      Serial.println("Start 1 Player");
     }
-    if (recievedMessage == 1) {
-        button = 0;
-        i = 0;
-        Serial.println("button 1 on");
-      }
-      else if (recievedMessage == 2) {
-        i = 0;
-        button = 1;
-        Serial.println("button 2 on");
-      }
-      else if (recievedMessage == 3) {
-        i = 0;
-        button = 2;
-        Serial.println("button 3 on");
-      }
+    if (game == 2) {
+      if (recievedMessage == 1) {
+          if (button != -1 && newButton) {
+            failed++;
+            }
+            button = 0;
+            Serial.println("button 1 on");
+            i = 0;
+           }
+          else if (recievedMessage == 2) {
+            if (button != -1 && newButton) {
+            failed++;
+            }
+            button = 1;
+            Serial.println("button 2 on");
+            i = 0;
+           }
+          else if (recievedMessage == 3) {
+            if (button != -1 && newButton) {
+            failed++;
+            }
+            button = 2;
+            Serial.println("button 3 on");
+            i = 0;
+          }
+          else if (recievedMessage == 4) {
+            if (button != -1 && newButton) {
+            failed++;
+            }
+            button = 3;
+            Serial.println("button 4 on");
+            i = 0;
+          }
+          else if (recievedMessage == 5) {
+            if (button != -1 && newButton) {
+            failed++;
+            }
+            button = 4;
+            Serial.println("button 5 on");
+            i = 0;
+          }
+          else if (recievedMessage == 6) {
+            if (button != -1 && newButton) {
+            failed++;
+            }
+            button = 5;
+            Serial.println("button 6 on");
+            i = 0;
+          }
+          else if (recievedMessage == 7) {
+            if (button != -1 && newButton) {
+            failed++;
+            }
+            button = 6;
+            Serial.println("button 7 on");
+            i = 0;
+          }
+          else if (recievedMessage == 8) {
+            if (button != -1 && newButton) {
+            failed++;
+            }
+            button = 7;
+            Serial.println("button 8 on");
+            i = 0;
+          }
+    }
   }
   if (SerialBT.available()) {
     newButton = false;
@@ -134,31 +206,74 @@ void loop() {
       failed = 0;
       i = 0;
       recievedMessage = 0;
+      Serial.println("Start 1 Player");
     }
-    if (recievedMessage == 1) {
-      if (button != -1 && newButton) {
-        failed++;
-        }
-        button = 0;
-        Serial.println("button 1 on");
-        i = 0;
-       }
-      else if (recievedMessage == 2) {
+    if (game == 2) {
+      if (recievedMessage == 1) {
         if (button != -1 && newButton) {
-        failed++;
+          failed++;
+          }
+          button = 0;
+          Serial.println("button 1 on");
+          i = 0;
+         }
+        else if (recievedMessage == 2) {
+          if (button != -1 && newButton) {
+          failed++;
+          }
+          button = 1;
+          Serial.println("button 2 on");
+          i = 0;
+         }
+        else if (recievedMessage == 3) {
+          if (button != -1 && newButton) {
+          failed++;
+          }
+          button = 2;
+          Serial.println("button 3 on");
+          i = 0;
         }
-        button = 1;
-        Serial.println("button 2 on");
-        i = 0;
-       }
-      else if (recievedMessage == 3) {
-        if (button != -1 && newButton) {
-        failed++;
+        else if (recievedMessage == 4) {
+          if (button != -1 && newButton) {
+          failed++;
+          }
+          button = 3;
+          Serial.println("button 4 on");
+          i = 0;
         }
-        button = 2;
-        Serial.println("button 3 on");
-        i = 0;
-      }
+        else if (recievedMessage == 5) {
+          if (button != -1 && newButton) {
+          failed++;
+          }
+          button = 4;
+          Serial.println("button 5 on");
+          i = 0;
+        }
+        else if (recievedMessage == 6) {
+          if (button != -1 && newButton) {
+          failed++;
+          }
+          button = 5;
+          Serial.println("button 6 on");
+          i = 0;
+        }
+        else if (recievedMessage == 7) {
+          if (button != -1 && newButton) {
+          failed++;
+          }
+          button = 6;
+          Serial.println("button 7 on");
+          i = 0;
+        }
+        else if (recievedMessage == 8) {
+          if (button != -1 && newButton) {
+          failed++;
+          }
+          button = 7;
+          Serial.println("button 8 on");
+          i = 0;
+        }
+    }
   }
 
 //1-player game
@@ -166,76 +281,120 @@ void loop() {
     button_0_pressed = digitalRead(BUTTON_0_IN);
     button_1_pressed = digitalRead(BUTTON_1_IN);
     button_2_pressed = digitalRead(BUTTON_2_IN);
+    button_3_pressed = digitalRead(BUTTON_3_IN);
+    button_4_pressed = digitalRead(BUTTON_4_IN);
+    button_5_pressed = digitalRead(BUTTON_5_IN);
+    button_6_pressed = digitalRead(BUTTON_6_IN);
+    button_7_pressed = digitalRead(BUTTON_7_IN);
     if (i < reaction_time) {
-      if (button == 0) {
-        digitalWrite(BUTTON_0_OUT, HIGH);
-        digitalWrite(BUTTON_1_OUT, LOW);
-        digitalWrite(BUTTON_2_OUT, LOW);
-      }
-      else if (button == 1) {
-        digitalWrite(BUTTON_1_OUT, HIGH);
-        digitalWrite(BUTTON_0_OUT, LOW);
-        digitalWrite(BUTTON_2_OUT, LOW);
-      }
-      else if (button == 2) {
-        digitalWrite(BUTTON_0_OUT, LOW);
-        digitalWrite(BUTTON_1_OUT, LOW);
-        digitalWrite(BUTTON_2_OUT, HIGH);
-      }
+      button_lighting();
+      
       if (button_0_pressed == HIGH) {
         if (button == 0) {
-          i = 0;
           digitalWrite(BUTTON_0_OUT, LOW);
-          score++;
-          delay(delay_time);
-          button = random(3);
-          delay(25);
+          button_correct();
+          button = random(8);
         }
         else {
           button_miss();
-          button = random(3);
+          button = random(8);
           delay(25);
         }
       }
-      if (button_1_pressed == HIGH) {
+      else if (button_1_pressed == HIGH) {
         if (button == 1) {
           i = 0;
           digitalWrite(BUTTON_1_OUT, LOW);
-          delay(delay_time);
-          score++;
-          button = random(3);
-          delay(25);
+          button_correct();
+          button = random(8);
         }
         else{
           button_miss();
-          button = random(3);
+          button = random(8);
           delay(25);
         }
       }
-      if (button_2_pressed == HIGH){
+      else if (button_2_pressed == HIGH){
         if (button == 2) {
           i = 0;
           digitalWrite(BUTTON_2_OUT, LOW);
-          delay(delay_time);
-          score++;
-          button = random(3);
-          delay(25);
+          button_correct();
+          button = random(8);
         }
         else{
           button_miss();
-          button = random(3);
+          button = random(8);
           delay(25);
         }
       }
+      else if (button_3_pressed == HIGH) {
+        if (button == 3) {
+          i = 0;
+          digitalWrite(BUTTON_3_OUT, LOW);
+          button_correct();
+          button = random(8);
+        }
+        else{
+          button_miss();
+          button = random(8);
+          delay(25);
+        }
+      }
+      else if (button_4_pressed == HIGH) {
+        if (button == 4) {
+          i = 0;
+          digitalWrite(BUTTON_4_OUT, LOW);
+          button_correct();
+          button = random(8);
+        }
+        else{
+          button_miss();
+          button = random(8);
+          delay(25);
+        }
+      }
+      else if (button_5_pressed == HIGH) {
+        if (button == 5) {
+          i = 0;
+          digitalWrite(BUTTON_5_OUT, LOW);
+          button_correct();
+          button = random(8);
+        }
+        else{
+          button_miss();
+          button = random(8);
+          delay(25);
+        }
+      }
+      else if (button_6_pressed == HIGH) {
+        if (button == 6) {
+          i = 0;
+          digitalWrite(BUTTON_6_OUT, LOW);
+          button_correct();
+          button = random(8);
+        }
+        else{
+          button_miss();
+          button = random(8);
+          delay(25);
+        }
+      }
+      else if (button_7_pressed == HIGH) {
+        if (button == 7) {
+          i = 0;
+          digitalWrite(BUTTON_7_OUT, LOW);
+          button_correct();
+          button = random(8);
+        }
+        else{
+          button_miss();
+          button = random(8);
+          delay(25);
+        }
+      }
+        
       i++;
       //Serial.println(i);
-    }
-    else if (i == reaction_time) {
-    SerialBT.write('2');
-    digitalWrite(BUTTON_0_OUT, LOW);
-    digitalWrite(BUTTON_1_OUT, LOW);
-    digitalWrite(BUTTON_2_OUT, LOW);
-    i++;
     }
   }
 
@@ -245,76 +404,112 @@ void loop() {
     button_0_pressed = digitalRead(BUTTON_0_IN);
     button_1_pressed = digitalRead(BUTTON_1_IN);
     button_2_pressed = digitalRead(BUTTON_2_IN);
+    button_3_pressed = digitalRead(BUTTON_3_IN);
+    button_4_pressed = digitalRead(BUTTON_4_IN);
+    button_5_pressed = digitalRead(BUTTON_5_IN);
+    button_6_pressed = digitalRead(BUTTON_6_IN);
+    button_7_pressed = digitalRead(BUTTON_7_IN);
     
     if (i < reaction_time) {
-      Serial.print(button);
-      Serial.println(" should be on");
-      if (button == 0) {
-        digitalWrite(BUTTON_0_OUT, HIGH);
-        digitalWrite(BUTTON_1_OUT, LOW);
-        digitalWrite(BUTTON_2_OUT, LOW);
-      }
-      else if (button == 1) {
-        digitalWrite(BUTTON_1_OUT, HIGH);
-        digitalWrite(BUTTON_0_OUT, LOW);
-        digitalWrite(BUTTON_2_OUT, LOW);
-      }
-      else if (button == 2) {
-        digitalWrite(BUTTON_0_OUT, LOW);
-        digitalWrite(BUTTON_1_OUT, LOW);
-        digitalWrite(BUTTON_2_OUT, HIGH);
-      }
+      button_lighting();
       if (button_0_pressed == HIGH) {
         if (button == 0) {
-          i = 0;
-          button = -1;
           digitalWrite(BUTTON_0_OUT, LOW);
-          score++;
+          button_correct();
           SerialBT.write(score);
-          delay(500);
         }
         else {
           button_miss();
           button = -1;
+          delay(25);
         }
       }
-      if (button_1_pressed == HIGH) {
+      else if (button_1_pressed == HIGH) {
         if (button == 1) {
-          i = 0;
-          button = -1;
           digitalWrite(BUTTON_1_OUT, LOW);
-          delay(500);
-          score++;
+          button_correct();
           SerialBT.write(score);
         }
         else {
           button_miss();
           button = -1;
+          delay(25);
         }
       }
-      if (button_2_pressed == HIGH){
+      else if (button_2_pressed == HIGH){
         if (button == 2) {
-          i = 0;
-          button = -1;
           digitalWrite(BUTTON_2_OUT, LOW);
-          delay(500);
-          score++;
+          button_correct();
           SerialBT.write(score);
         }
         else {
           button_miss();
           button = -1;
+          delay(25);
+        }
+      }
+      else if (button_3_pressed == HIGH){
+        if (button == 3) {
+          digitalWrite(BUTTON_3_OUT, LOW);
+          button_correct();
+          SerialBT.write(score);
+        }
+        else {
+          button_miss();
+          button = -1;
+          delay(25);
+        }
+      }
+      else if (button_4_pressed == HIGH){
+        if (button == 4) {
+          digitalWrite(BUTTON_4_OUT, LOW);
+          button_correct();
+          SerialBT.write(score);
+        }
+        else {
+          button_miss();
+          button = -1;
+          delay(25);
+        }
+      }
+      else if (button_5_pressed == HIGH){
+        if (button == 5) {
+          digitalWrite(BUTTON_5_OUT, LOW);
+          button_correct();
+          SerialBT.write(score);
+        }
+        else {
+          button_miss();
+          button = -1;
+          delay(25);
+        }
+      }
+      else if (button_6_pressed == HIGH){
+        if (button == 6) {
+          digitalWrite(BUTTON_6_OUT, LOW);
+          button_correct();
+          SerialBT.write(score);
+        }
+        else {
+          button_miss();
+          button = -1;
+          delay(25);
+        }
+      }
+      else if (button_7_pressed == HIGH){
+        if (button == 7) {
+          digitalWrite(BUTTON_7_OUT, LOW);
+          button_correct();
+          SerialBT.write(score);
+        }
+        else {
+          button_miss();
+          button = -1;
+          delay(25);
         }
       }
       i++;
       //Serial.println(i);
-    }
-    else if (i == reaction_time) {
-    SerialBT.write('g');
-    digitalWrite(BUTTON_0_OUT, LOW);
-    digitalWrite(BUTTON_1_OUT, LOW);
-    digitalWrite(BUTTON_2_OUT, LOW);
-    i++;
     }
   }
   
@@ -323,19 +518,6 @@ void loop() {
 void button_miss() {
      failed += 1;
      SerialBT.write('m');
-     /*
-     digitalWrite(BUTTON_0_OUT, LOW);
-     digitalWrite(BUTTON_1_OUT, LOW);
-     digitalWrite(BUTTON_2_OUT, LOW);
-     delay(100);
-     digitalWrite(BUTTON_0_OUT, HIGH);
-     digitalWrite(BUTTON_1_OUT, HIGH);
-     digitalWrite(BUTTON_2_OUT, HIGH);
-     delay(100);
-     digitalWrite(BUTTON_0_OUT, LOW);
-     digitalWrite(BUTTON_1_OUT, LOW);
-     digitalWrite(BUTTON_2_OUT, LOW);
-     */
 }
 
 void button_correct() {
@@ -346,4 +528,100 @@ void button_correct() {
   if (score % 5 == 0) {
     reaction_time -= 500;
   }
+}
+
+void game_over() {
+  SerialBT.write('g');
+  game = 0;
+  digitalWrite(BUTTON_0_OUT, LOW);
+  digitalWrite(BUTTON_1_OUT, LOW);
+  digitalWrite(BUTTON_2_OUT, LOW);
+  digitalWrite(BUTTON_3_OUT, LOW);
+  digitalWrite(BUTTON_4_OUT, LOW);
+  digitalWrite(BUTTON_5_OUT, LOW);
+  digitalWrite(BUTTON_6_OUT, LOW);
+  digitalWrite(BUTTON_7_OUT, LOW);
+}
+
+void button_lighting() {
+  if (button == 0) {
+        digitalWrite(BUTTON_0_OUT, HIGH);
+        digitalWrite(BUTTON_1_OUT, LOW);
+        digitalWrite(BUTTON_2_OUT, LOW);
+        digitalWrite(BUTTON_3_OUT, LOW);
+        digitalWrite(BUTTON_4_OUT, LOW);
+        digitalWrite(BUTTON_5_OUT, LOW);
+        digitalWrite(BUTTON_6_OUT, LOW);
+        digitalWrite(BUTTON_7_OUT, LOW);
+      }
+      else if (button == 1) {
+        digitalWrite(BUTTON_0_OUT, LOW);
+        digitalWrite(BUTTON_1_OUT, HIGH);
+        digitalWrite(BUTTON_2_OUT, LOW);
+        digitalWrite(BUTTON_3_OUT, LOW);
+        digitalWrite(BUTTON_4_OUT, LOW);
+        digitalWrite(BUTTON_5_OUT, LOW);
+        digitalWrite(BUTTON_6_OUT, LOW);
+        digitalWrite(BUTTON_7_OUT, LOW);
+      }
+      else if (button == 2) {
+        digitalWrite(BUTTON_0_OUT, LOW);
+        digitalWrite(BUTTON_1_OUT, LOW);
+        digitalWrite(BUTTON_2_OUT, HIGH);
+        digitalWrite(BUTTON_3_OUT, LOW);
+        digitalWrite(BUTTON_4_OUT, LOW);
+        digitalWrite(BUTTON_5_OUT, LOW);
+        digitalWrite(BUTTON_6_OUT, LOW);
+        digitalWrite(BUTTON_7_OUT, LOW);
+      }
+      else if (button == 3) {
+        digitalWrite(BUTTON_0_OUT, LOW);
+        digitalWrite(BUTTON_1_OUT, LOW);
+        digitalWrite(BUTTON_2_OUT, LOW);
+        digitalWrite(BUTTON_3_OUT, HIGH);
+        digitalWrite(BUTTON_4_OUT, LOW);
+        digitalWrite(BUTTON_5_OUT, LOW);
+        digitalWrite(BUTTON_6_OUT, LOW);
+        digitalWrite(BUTTON_7_OUT, LOW);
+      }
+      else if (button == 4) {
+        digitalWrite(BUTTON_0_OUT, LOW);
+        digitalWrite(BUTTON_1_OUT, LOW);
+        digitalWrite(BUTTON_2_OUT, LOW);
+        digitalWrite(BUTTON_3_OUT, LOW);
+        digitalWrite(BUTTON_4_OUT, HIGH);
+        digitalWrite(BUTTON_5_OUT, LOW);
+        digitalWrite(BUTTON_6_OUT, LOW);
+        digitalWrite(BUTTON_7_OUT, LOW);
+      }
+      else if (button == 5) {
+        digitalWrite(BUTTON_0_OUT, LOW);
+        digitalWrite(BUTTON_1_OUT, LOW);
+        digitalWrite(BUTTON_2_OUT, LOW);
+        digitalWrite(BUTTON_3_OUT, LOW);
+        digitalWrite(BUTTON_4_OUT, LOW);
+        digitalWrite(BUTTON_5_OUT, HIGH);
+        digitalWrite(BUTTON_6_OUT, LOW);
+        digitalWrite(BUTTON_7_OUT, LOW);
+      }
+      else if (button == 6) {
+        digitalWrite(BUTTON_0_OUT, LOW);
+        digitalWrite(BUTTON_1_OUT, LOW);
+        digitalWrite(BUTTON_2_OUT, LOW);
+        digitalWrite(BUTTON_3_OUT, LOW);
+        digitalWrite(BUTTON_4_OUT, LOW);
+        digitalWrite(BUTTON_5_OUT, LOW);
+        digitalWrite(BUTTON_6_OUT, HIGH);
+        digitalWrite(BUTTON_7_OUT, LOW);
+      }
+      else if (button == 7) {
+        digitalWrite(BUTTON_0_OUT, LOW);
+        digitalWrite(BUTTON_1_OUT, LOW);
+        digitalWrite(BUTTON_2_OUT, LOW);
+        digitalWrite(BUTTON_3_OUT, LOW);
+        digitalWrite(BUTTON_4_OUT, LOW);
+        digitalWrite(BUTTON_5_OUT, LOW);
+        digitalWrite(BUTTON_6_OUT, LOW);
+        digitalWrite(BUTTON_7_OUT, HIGH);
+      }
 }
